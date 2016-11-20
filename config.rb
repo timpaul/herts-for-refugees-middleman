3d2087acae504fdd5f2b36e2853dc4e52fb0d36a
Tim Paul <contact@timpaul.co.uk>
2016-11-20 10:48:07 +0000

    Use deploy_method instead of method

--- config.rb
+++ config.rb
@@ -68,7 +68,7 @@ configure :build do
 end
 
 activate :deploy do |deploy|
-  deploy.method = :git
+  deploy.deploy_method = :git
   deploy.branch = 'gh-pages' # or master
 
   committer_app = "#{Middleman::Deploy::PACKAGE} v#{Middleman::Deploy::VERSION}"
